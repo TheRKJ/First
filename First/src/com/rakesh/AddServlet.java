7386592aa972365e0e6308c7ec61aa3c6ee50a26
mss2015 <mss2015@mss2015>
2020-05-19 11:43:36 +0530

    Project added

---
+++ First/src/com/rakesh/AddServlet.java
@@ -0,0 +1,24 @@
+package com.rakesh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+   public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	   
+	   int i = Integer.parseInt(req.getParameter("num1"));
+	   int j = Integer.parseInt(req.getParameter("num2"));
+	   
+	   int k = i+j;
+	   
+	   PrintWriter out = res.getWriter();
+	   
+	   out.println("<html><body bgcolor='cyan'>");
+	   out.println("<h1>Result is "+k+"</h1>");
+	   out.println("</body></html>");
+   }
+}
